02a20d51970e7e64e42918b64eef137552008d2e
TungDVS <dangvusontung@gmail.com>
2023-11-11 00:44:27 +0700

    Initial Commit

---
+++ Language Learning/Language_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language_LearningApp.swift
+//  Language Learning
+//
+//  Created by TungDVS on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Language_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
